53fda4690d808b8a972660b9d74ec3ec3cbb6326
Jan Vogt <news.jan.vogt@me.com>
2013-05-01 19:54:04 +0200

    Add Library Item Class

--- insert_citation.py
+++ insert_citation.py
@@ -33,6 +33,41 @@ from pyzotero import zotero
 class InsertCitationCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         zot = zotero.Zotero('1197522', 'user', 'FUWTcdnRvb1bFy8ugDudDDhA')
-        first_ten = zot.items(limit=10)
-        print first_ten
+        first_ten = zot.top()
+        i = 1
+        for item in first_ten:
+            print "Eintrag %d" % i
+            i += 1
+            libI = LibraryItem('1197522', item)
+            print libI
+            #for key in item.keys():
+            #    try:
+            #        print u"\t%s = '%s'" % (key, item[key])
+            #    except UnicodeEncodeError:
+            #        print u"\t%s = '%s'" % (key, "Unicode Error")
         self.view.insert(edit, 0, "Hello, World!")
+
+
+class LibraryItem(object):
+    __zoteroInstances = {}
+
+    def __init__(self, libId, libDict):
+        self.id = libDict[u'key']
+        self.libId = libId
+        self.authors = self.__decodeAuthors(libDict[u'creators'])
+        self.title = libDict[u'title']
+        LibraryItem.__zoteroInstances["test"] = zotero.Zotero('1197522', 'user', 'FUWTcdnRvb1bFy8ugDudDDhA')
+
+    def __decodeAuthors(self, cratorsDict):
+        retVal = u""
+        seperator = u""
+        for creator in cratorsDict:
+            retVal += (u"%s%s, %s" % (seperator, creator[u'lastName'], creator[u'firstName'][0]))
+            seperator = u'; '
+        return retVal
+
+    def __str__(self):
+        return str(self.__unicode__())
+
+    def __unicode__(self):
+        return u"LibraryItem<%s in %s>(%s: \"%s\")" % (self.id, self.libId, self.authors, self.title)
