9dd60babbd336f1d771bc7c4dfbcd8528c95ea50
Jan Vogt <news.jan.vogt@me.com>
2013-05-03 19:05:28 +0200

    Add Library Object for data management and update it asynchronously

--- insert_citation.py
+++ insert_citation.py
@@ -1,210 +1,58 @@
 import sublime
 import sublime_plugin
-import os
-import sys
-import re
-import codecs
-
-if os.name == 'nt':
-    from ctypes import windll, create_unicode_buffer
-
-
-def add_to_path(path):
-    # Python 2.x on Windows can't properly import from non-ASCII paths, so
-    # this code added the DOC 8.3 version of the lib folder to the path in
-    # case the user's username includes non-ASCII characters
-    if os.name == 'nt':
-        buf = create_unicode_buffer(512)
-        if windll.kernel32.GetShortPathNameW(path, buf, len(buf)):
-            path = buf.value
-
-    if path not in sys.path:
-        sys.path.append(path)
-
-lib_folder = os.path.join(sublime.packages_path(), 'ZoteroCite', 'lib')
-add_to_path(os.path.join(lib_folder, 'pyzotero'))
-add_to_path(os.path.join(lib_folder, 'feedparser'))
-add_to_path(os.path.join(lib_folder, 'pytz-2013b'))
-add_to_path(os.path.join(lib_folder, 'poster-0.8.1'))
-add_to_path(os.path.join(lib_folder, 'ordereddict-1.1'))
-
-
-from pyzotero import zotero
+from library import Library
+import threading
 
 
 class InsertCitationCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        #first_ten = LibraryItem.getAllUserItems('1197522', 'FUWTcdnRvb1bFy8ugDudDDhA')
-        bibTexEntries = BibTexEntry.readFromFile("/Users/jan/Desktop/test.bib")
-        first_ten = [LibraryItem.createFromBibTexEntry(entry, 'FUWTcdnRvb1bFy8ugDudDDhA') for entry in bibTexEntries]
-        selectFrom = []
-        for item in first_ten:
-            selectFrom.append([item.authors, item.title])
+        library = self.getLibrary()
+        self.selectionList = library.LibraryItems
+        self.selectionList.sort(key=lambda x: x.menuRows[0])
+        selectFrom = [item.menuRows for item in self.selectionList]
         #BibTexEntry.writeToFile([item.bibTexEntry for item in first_ten], "/Users/jan/Desktop/test.bib")
         self.view.window().show_quick_panel(selectFrom, self.callBack)
         self.view.insert(edit, 0, "Hello, World!")
 
-    def callBack(self, arg):
-        print arg
-
-
-class LibraryItem(object):
-    __zoteroInstances = {}
-
-    def __init__(self, docId, zotInstance, authors, title, bibTexEntry=None):
-        """Private Constructor.
-        Don't use to create instances. Use getAllUserItems or getAllGroupItems instead"""
-        self.id = docId
-        self.zotInstance = zotInstance
-        self.authors = authors
-        self.title = title
-        self.__bibTexEntry = bibTexEntry
-
-    @staticmethod
-    def __initFromZotero(zotInstance, libItemDict):
-        """Private Constructor.
-        Don't use to create instances. Use getAllUserItems or getAllGroupItems instead"""
-        return LibraryItem(
-            libItemDict[u'key'],
-            zotInstance,
-            LibraryItem.__decodeAuthors(libItemDict[u'creators']),
-            libItemDict[u'title'],
-            None)
-
-    @staticmethod
-    def __decodeAuthors(cratorsDict):
-        retVal = u""
-        seperator = u""
-        for creator in cratorsDict:
-            retVal += (u"%s%s, %s" % (seperator, creator[u'lastName'], creator[u'firstName'].split()[0]))
-            seperator = u'; '
-        return retVal
-
-    def __str__(self):
-        return self.__unicode__().encode("ascii", "replace")
-
-    def __unicode__(self):
-        return u"LibraryItem<%s in %s>(%s: \"%s\")" % (self.id, "%s(%s)" % self.zotInstance, self.authors, self.title)
-
-    @property
-    def bibTexEntry(self):
-        if self.__bibTexEntry is None:
-            self.__bibTexEntry = BibTexEntry(self.__zoteroInstances[self.zotInstance].item(self.id, content='bibtex')[0])
-            self.__bibTexEntry.zoteroLink(self.id, self.zotInstance[0], self.zotInstance[1])
-        return self.__bibTexEntry
-
-    @classmethod
-    def __addZoteroInstance(cls, libId, libType, key):
-        libId = unicode(libId)
-        libType = unicode(libType)
-        key = unicode(key)
-        zotInstance = (libId, libType)
-        if zotInstance not in cls.__zoteroInstances.keys():
-            cls.__zoteroInstances[zotInstance] = zotero.Zotero(libId, libType, key)
-        return zotInstance
-
-    @classmethod
-    def getAllUserItems(cls, userId, key):
-        zotInstance = cls.__addZoteroInstance(userId, "user", key)
-        retVal = []
-        for libItemDict in cls.__zoteroInstances[zotInstance].top():
-            retVal.append(cls.__initFromZotero(zotInstance, libItemDict))
-        for group in cls.__zoteroInstances[zotInstance].groups():
-            retVal += LibraryItem.getAllGroupItems(group[u'group_id'], key)
-        return retVal
-
-    @classmethod
-    def getAllGroupItems(cls, groupId, key):
-        zotInstance = cls.__addZoteroInstance(groupId, "group", key)
-        retVal = []
-        for libItemDict in cls.__zoteroInstances[zotInstance].top():
-            retVal.append(cls.__initFromZotero(zotInstance, libItemDict))
-        for group in cls.__zoteroInstances[zotInstance].groups():
-            retVal += LibraryItem.getAllGroupItems(group[u'group_id'], key)
-        return retVal
-
-    @classmethod
-    def createFromBibTexEntry(cls, bibTexEntry, zoteroKey):
+    def getLibrary(self):
         try:
-            zoteroInstance = cls.__addZoteroInstance(bibTexEntry.zoteroLibraryId, bibTexEntry.zoteroLibraryType, zoteroKey)
-            itemDict = cls.__zoteroInstances[zoteroInstance].item(bibTexEntry.zoteroKey)[0]
-        except IOError:
-            return LibraryItem(
-                None,
-                None,
-                bibTexEntry.entrys.get("author", "No Author(s)"),
-                bibTexEntry.entrys.get("title", "No Title"),
-                bibTexEntry)
-        else:
-            retInstance = cls.__initFromZotero(zoteroInstance, itemDict)
-            if bibTexEntry.key is not retInstance.bibTexEntry.key:
-                #BibTex-Key has changed: Do Somethin to change Keys in Document
-                pass
-            return retInstance
+            self.__library
+        except AttributeError:
+            self.__library = Library.getLibraryForView(self.view)
+        return self.__library
+
+    def callBack(self, arg):
+        print self.getLibrary().cite(self.selectionList[arg])
 
 
-class BibTexEntry(object):
-    __valueInCurlyBraces = re.compile("\\{(.*)\\}", re.S)
+class PluginEventHandler(sublime_plugin.EventListener):
+    def on_activated(self, view):
+        if Library.hasLibraryForView(view):
+            UpdateThread(Library.getLibraryForView(view)).start()
 
-    def __init__(self, bibTexString):
-        bibTexString = unicode(bibTexString)
-        bibTexEntryMatch = re.search("@(\\S+?)\\s*\\{\\s*(\\S+?)\\s*,(.*)\\}", bibTexString, re.S)
-        if bibTexEntryMatch:
-            self.type = bibTexEntryMatch.group(1)
-            self.key = bibTexEntryMatch.group(2)
-            self.entrys = {}
-            for match in re.finditer("(\\S+?)\\s*=\\s*(.*?),?\\n", bibTexEntryMatch.group(3)):
-                self.entrys[match.group(1)] = match.group(2)
-        else:
-            raise ValueError("Passed string doesn't contain a valid BiBTex-Entry")
 
-    @property
-    def bibTexString(self):
-        entrysString = ""
-        for key in self.entrys.keys():
-            entrysString += "\t%s = %s,\n" % (key, self.entrys[key])
-        return u"@%s{%s,\n%s}" % (self.type, self.key, entrysString)
+class UpdateThread(threading.Thread):
+    __updateLock = threading.Lock()
 
-    @property
-    def zoteroKey(self):
-        return self.__valueInCurlyBraces.search(self.entrys.get("zoterodocid", None)).group(1)
+    def __init__(self, lib):
+        self.lib = lib
+        self.done = False
+        self.__points = 0
+        super(UpdateThread, self).__init__()
 
-    @property
-    def zoteroLibraryId(self):
-        return self.__valueInCurlyBraces.search(self.entrys.get("zoterolibid", None)).group(1)
+    def run(self):
+        if self.__updateLock.acquire(False):
+            sublime.set_timeout(self.update_status, 300)
+            try:
+                self.lib.update()
+            finally:
+                self.__updateLock.release()
+                self.done = True
 
-    @property
-    def zoteroLibraryType(self):
-        return self.__valueInCurlyBraces.search(self.entrys.get("zoterolibtype", None)).group(1)
-
-    def zoteroLink(self, key, libId, libType):
-        self.entrys["zoterodocid"] = "{%s}" % key
-        self.entrys["zoterolibid"] = "{%s}" % libId
-        self.entrys["zoterolibtype"] = "{%s}" % libType
-
-    @staticmethod
-    def readFromFile(filePath):
-        with codecs.open(filePath, "r", "utf-8") as f:
-            bibTexString = f.read()
-        startIndex = 0
-        braceLevel = 0
-        entry = False
-        retVal = []
-        for i in xrange(0, len(bibTexString)):
-            if entry is True:
-                if bibTexString[i] == u"{":
-                    braceLevel += 1
-                elif bibTexString[i] == u"}":
-                    braceLevel -= 1
-                    if braceLevel == 0:
-                        entry = False
-                        retVal.append(BibTexEntry(bibTexString[startIndex:i+1]))
-            elif bibTexString[i] == u"@":
-                entry = True
-                startIndex = i
-        return retVal
-
-    @staticmethod
-    def writeToFile(bibTexEntries, filePath):
-        with codecs.open(filePath, "w", "utf-8") as f:
-            f.writelines([entry.bibTexString + "\n" for entry in bibTexEntries])
+    def update_status(self):
+        if self.done:
+            sublime.status_message("")
+            return
+        self.__points = self.__points % 3 + 1
+        sublime.status_message("Updating Library" + self.__points*".")
+        sublime.set_timeout(self.update_status, 300)
---
+++ library.py
@@ -0,0 +1,292 @@
+import sublime
+import os
+import sys
+import re
+import codecs
+import threading
+
+if os.name == 'nt':
+    from ctypes import windll, create_unicode_buffer
+
+
+def add_to_path(path):
+    # Python 2.x on Windows can't properly import from non-ASCII paths, so
+    # this code added the DOC 8.3 version of the lib folder to the path in
+    # case the user's username includes non-ASCII characters
+    if os.name == 'nt':
+        buf = create_unicode_buffer(512)
+        if windll.kernel32.GetShortPathNameW(path, buf, len(buf)):
+            path = buf.value
+
+    if path not in sys.path:
+        sys.path.append(path)
+
+lib_folder = os.path.join(sublime.packages_path(), 'ZoteroCite', 'lib')
+add_to_path(os.path.join(lib_folder, 'pyzotero'))
+add_to_path(os.path.join(lib_folder, 'feedparser'))
+add_to_path(os.path.join(lib_folder, 'pytz-2013b'))
+add_to_path(os.path.join(lib_folder, 'poster-0.8.1'))
+add_to_path(os.path.join(lib_folder, 'ordereddict-1.1'))
+
+
+from pyzotero import zotero
+
+
+class Library(object):
+    __instances = {}
+
+    def __init__(self, view, zotLibId, zotLibKey, pathToBibFile=None):
+        """Initializes a library based on a Zoteros user library and an optional BibTex-file"""
+        self.__instances[view.buffer_id()] = self
+        self.__pathToBibFile = pathToBibFile
+        self.__zoteroInstances = {}
+        self.__libItems = []
+        self.__rootZoteroCredentials = (zotLibId, zotLibKey)
+        self.__libLock = threading.RLock()
+        self.__zoteroLock = threading.RLock()
+        self.update()
+
+    @property
+    def LibraryItems(self):
+        with self.__libLock:
+            return list(self.__libItems)
+
+    @property
+    def pathToBibFile(self):
+        return self.__pathToBibFile
+
+    @pathToBibFile.setter
+    def pathToBibFile(self, value):
+        raise NotImplementedError
+
+    def update(self):
+        """Collect all items from Zotero and if it exists unions them with those from
+        the BibTex-file. In case of matches it assumes Zoteros' version to be the correct one"""
+        newItems = []
+        if self.pathToBibFile is not None:
+            bibTexEntries = self.__readFromBibFile(self.pathToBibFile)
+            newItems.extend(
+                [LibraryItem(
+                    entry.zoteroKey,
+                    None,
+                    entry.entrys.get("author", "No Author(s)"),
+                    entry.entrys.get("title", "No Title"),
+                    entry,
+                    True
+                ) for entry in bibTexEntries]
+            )
+        newItems.extend(self.__getAllUserItems(*self.__rootZoteroCredentials))
+        with self.__libLock:
+            for item in newItems:
+                if item in self.__libItems:
+                    index = self.__libItems.index(item)
+                    if self.__libItems[index].cited:
+                        if item.bibTexEntry is None:
+                            item.bibTexEntry = self.__bibTexEntryForLibItem(item)
+                        #The cited one has a bibTexEntry
+                        if self.__libItems[index].bibTexEntry.key != item.bibTexEntry.key:
+                            print "Warning: Citekey %s changed to %s" % (self.__libItems[index].bibTexEntry.key, item.bibTexEntry.key)
+                            raise NotImplementedError
+                            # Do sth. like replace the old keys
+                    self.__libItems[index] = item
+                else:
+                    self.__libItems.append(item)
+
+    def save(self):
+        if self.pathToBibFile is not None:
+            self.__writeToBibFile([item.bibTexEntry for item in self.libItems if item.cited], self.pathToBibFile)
+            return True
+        else:
+            return False
+
+    def cite(self, libItem):
+        if libItem.bibTexEntry is None:
+            libItem.bibTexEntry = self.__bibTexEntryForLibItem(libItem)
+        return libItem.bibTexEntry.key
+
+    @classmethod
+    def getLibraryForView(cls, view):
+        try:
+            return cls.__instances[view.buffer_id()]
+        except KeyError:
+            settings = sublime.load_settings("ZoteroCite.sublime-settings")
+            filename = None
+            if view.file_name() is not None:
+                filename = os.path.splitext(view.file_name())[0] + '.bib'
+            return Library(view, settings.get("zotero_user_id"), settings.get("zotero_user_key"), filename)
+
+    @classmethod
+    def hasLibraryForView(cls, view):
+        try:
+            cls.__instances[view.buffer_id()]
+            return True
+        except KeyError:
+            return False
+
+    def __bibTexEntryForLibItem(self, libItem):
+        """creates an corresponding BibTexEntry for the given Library Item. If it is not possible to
+        retrieve a new one None is returned"""
+        with self.__zoteroLock:
+            try:
+                bibTexEntry = BibTexEntry(self.__zoteroInstances[libItem.zotInstance].item(libItem.id, content='bibtex')[0])
+            except AttributeError:
+                return libItem.bibTexEntry
+            else:
+                bibTexEntry.zoteroLink(libItem.id, libItem.zotInstance[0], libItem.zotInstance[1])
+                return bibTexEntry
+
+    def __addZoteroInstance(self, libId, libType, key=None):
+        zotInstanceIdentifier = (libId, libType)
+        if zotInstanceIdentifier not in self.__zoteroInstances.keys():
+            self.__zoteroInstances[zotInstanceIdentifier] = zotero.Zotero(libId, libType, key)
+        return zotInstanceIdentifier
+
+    def __getAllUserItems(self, userId, key):
+        zotInstanceIdentifier = self.__addZoteroInstance(userId, "user", key)
+        retVal = []
+        with self.__zoteroLock:
+            libItems = self.__zoteroInstances[zotInstanceIdentifier].top()
+        for libItemDict in libItems:
+            retVal.append(LibraryItem.initFromZotero(zotInstanceIdentifier, libItemDict))
+        with self.__zoteroLock:
+            groups = self.__zoteroInstances[zotInstanceIdentifier].groups()
+        for group in groups:
+            retVal += self.__getAllGroupItems(group[u'group_id'], key)
+        return retVal
+
+    def __getAllGroupItems(self, groupId, key):
+        zotInstanceIdentifier = self.__addZoteroInstance(groupId, "group", key)
+        retVal = []
+        with self.__zoteroLock:
+            libItems = self.__zoteroInstances[zotInstanceIdentifier].top()
+        for libItemDict in libItems:
+            retVal.append(LibraryItem.initFromZotero(zotInstanceIdentifier, libItemDict))
+        with self.__zoteroLock:
+            groups = self.__zoteroInstances[zotInstanceIdentifier].groups()
+        for group in groups:
+            retVal += self.__getAllGroupItems(group[u'group_id'], key)
+        return retVal
+
+    @staticmethod
+    def __readFromBibFile(filePath):
+        with codecs.open(filePath, "r", "utf-8") as f:
+            bibTexString = f.read()
+        startIndex = 0
+        braceLevel = 0
+        entry = False
+        retVal = []
+        for i in xrange(0, len(bibTexString)):
+            if entry is True:
+                if bibTexString[i] == u"{":
+                    braceLevel += 1
+                elif bibTexString[i] == u"}":
+                    braceLevel -= 1
+                    if braceLevel == 0:
+                        entry = False
+                        retVal.append(BibTexEntry(bibTexString[startIndex:i+1]))
+            elif bibTexString[i] == u"@":
+                entry = True
+                startIndex = i
+        return retVal
+
+    @staticmethod
+    def __writeToBibFile(bibTexEntries, filePath):
+        with codecs.open(filePath, "w", "utf-8") as f:
+            f.writelines([entry.bibTexString + "\n" for entry in bibTexEntries])
+
+
+class LibraryItem(object):
+    def __init__(self, docId, zotInstance, authors, title, bibTexEntry=None):
+        self.id = docId
+        self.zotInstance = zotInstance
+        self.authors = authors
+        self.title = title
+        self.bibTexEntry = bibTexEntry
+
+    @property
+    def cited(self):
+        return self.bibTexEntry is not None
+
+    @property
+    def menuRows(self):
+        return [self.authors, self.title]
+
+    def __str__(self):
+        return self.__unicode__().encode("ascii", "replace")
+
+    def __unicode__(self):
+        return u"LibraryItem<%s in %s>(%s: \"%s\")" % (self.id, "%s(%s)" % self.zotInstance, self.authors, self.title)
+
+    def __eq__(self, other):
+        if self.id is not None and other.id is not None:
+            return self.id == other.id
+        else:
+            try:
+                return self.bibTexEntry.key == other.bibTexEntry.key
+            except AttributeError:
+                return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    @staticmethod
+    def initFromZotero(zotInstance, libItemDict):
+        try:
+            return LibraryItem(
+                libItemDict[u'key'],
+                zotInstance,
+                LibraryItem.__decodeAuthors(libItemDict[u'creators']),
+                libItemDict[u'title'])
+        except TypeError as T:
+            print "TypeError: %s" % type(libItemDict)
+            print libItemDict
+            raise T
+
+    @staticmethod
+    def __decodeAuthors(cratorsDict):
+        retVal = u""
+        seperator = u""
+        for creator in cratorsDict:
+            retVal += (u"%s%s, %s" % (seperator, creator[u'lastName'], creator[u'firstName'].split()[0]))
+            seperator = u'; '
+        return retVal
+
+
+class BibTexEntry(object):
+    __valueInCurlyBraces = re.compile("\\{(.*)\\}", re.S)
+
+    def __init__(self, bibTexString):
+        bibTexString = unicode(bibTexString)
+        bibTexEntryMatch = re.search("@(\\S+?)\\s*\\{\\s*(\\S+?)\\s*,(.*)\\}", bibTexString, re.S)
+        if bibTexEntryMatch:
+            self.type = bibTexEntryMatch.group(1)
+            self.key = bibTexEntryMatch.group(2)
+            self.entrys = {}
+            for match in re.finditer("(\\S+?)\\s*=\\s*(.*?),?\\n", bibTexEntryMatch.group(3)):
+                self.entrys[match.group(1)] = match.group(2)
+        else:
+            raise ValueError("Passed string doesn't contain a valid BiBTex-Entry")
+
+    @property
+    def bibTexString(self):
+        entrysString = ""
+        for key in self.entrys.keys():
+            entrysString += "\t%s = %s,\n" % (key, self.entrys[key])
+        return u"@%s{%s,\n%s}" % (self.type, self.key, entrysString)
+
+    @property
+    def zoteroKey(self):
+        return self.__valueInCurlyBraces.search(self.entrys.get("zoterodocid", "")).group(1)
+
+    @property
+    def zoteroLibraryId(self):
+        return self.__valueInCurlyBraces.search(self.entrys.get("zoterolibid", "")).group(1)
+
+    @property
+    def zoteroLibraryType(self):
+        return self.__valueInCurlyBraces.search(self.entrys.get("zoterolibtype", "")).group(1)
+
+    def zoteroLink(self, key, libId, libType):
+        self.entrys["zoterodocid"] = "{%s}" % key
+        self.entrys["zoterolibid"] = "{%s}" % libId
+        self.entrys["zoterolibtype"] = "{%s}" % libType
